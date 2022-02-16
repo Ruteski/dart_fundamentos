2c0a67996f19d313bfa2f767ded3c23193bae7b5
Lincoln Ruteski <lruteski@gmail.com>
2022-02-16 18:50:58 -0300

    variaveis parte 1

---
+++ lib/1_variaveis/1_variaveis.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: file_names
+
+void main(List<String> args) {
+  num qualquerNumero = 1;
+  num qualquerNumero2 = 1.1;
+
+  int idade = 38;
+
+  double valorIphone = 12.500;
+
+  String nomeCurso = 'Curso de Dart';
+
+  var qualquerCoisa = 37;
+
+  Object objetoQualquer = 1.2; // todo mundo é filho de object
+
+  dynamic dynamicQualquer = 1;
+  dynamicQualquer = 5.96;
+  dynamicQualquer = 'bla';
+}
